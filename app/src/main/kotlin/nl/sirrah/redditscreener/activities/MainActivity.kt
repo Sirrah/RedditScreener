c3dc886159a1d874b485812fd198bc2cc3c2cb55
Peter Oost <peteroost@sirrah.nl>
2016-03-12 10:49:59 +0100

    Basics for the LinkAdapter

--- app/src/main/kotlin/nl/sirrah/redditscreener/activities/MainActivity.kt
+++ app/src/main/kotlin/nl/sirrah/redditscreener/activities/MainActivity.kt
@@ -2,9 +2,13 @@ package nl.sirrah.redditscreener.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import nl.sirrah.redditscreener.R
+import nl.sirrah.redditscreener.adapters.LinkAdapter
 import nl.sirrah.redditscreener.api.RedditService
+import org.jetbrains.anko.find
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
@@ -18,11 +22,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val linksList: RecyclerView = find(R.id.links)
+        linksList.layoutManager = GridLayoutManager(this, 3)
+
         redditService = RedditService.create()
 
         redditService.listing("awww")
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { listing -> Log.d(TAG, "Received: $listing") }
+                .subscribe { listing ->
+                    Log.d(TAG, "Received: $listing")
+                    linksList.adapter = LinkAdapter(listing.children);
+                }
     }
 }
--- app/src/main/kotlin/nl/sirrah/redditscreener/adapters/LinkAdapter.kt
+++ app/src/main/kotlin/nl/sirrah/redditscreener/adapters/LinkAdapter.kt
@@ -1,7 +1,42 @@
 package nl.sirrah.redditscreener.adapters
 
-/**
- * Created by peter.oost on 2/17/16.
- */
-class LinkAdapter {
+import android.net.Uri
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import nl.sirrah.redditscreener.R
+import nl.sirrah.redditscreener.api.Link
+import org.jetbrains.anko.find
+
+class LinkAdapter(val data: List<Link>) : RecyclerView.Adapter<LinkAdapter.ViewHolder>() {
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        val itemView = inflater.inflate(R.layout.link_item, parent, false)
+
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(data[position]) {
+            val target : ImageView = holder.rootView.find(R.id.image)
+
+            val uri = Uri.parse(thumbnail)
+            Picasso.with(holder.rootView.context)
+                    .load(uri)
+                    .fit()
+                    .centerCrop()
+                    .error(android.R.drawable.btn_plus)
+                    .placeholder(android.R.drawable.btn_plus)
+                    .into(target)
+        }
+    }
+
+    class ViewHolder(val rootView: View) : RecyclerView.ViewHolder(rootView)
 }
