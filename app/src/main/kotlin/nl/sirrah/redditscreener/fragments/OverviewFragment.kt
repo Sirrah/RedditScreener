3342acd0886c99acd596dc04e4d8d0acd96b5300
Peter Oost <peteroost@sirrah.nl>
2016-06-13 22:06:10 +0200

    Use AnkoLogger trait

--- app/src/main/kotlin/nl/sirrah/redditscreener/fragments/OverviewFragment.kt
+++ app/src/main/kotlin/nl/sirrah/redditscreener/fragments/OverviewFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,14 +11,13 @@ import nl.sirrah.redditscreener.R
 import nl.sirrah.redditscreener.adapters.LinkAdapter
 import nl.sirrah.redditscreener.api.RedditService
 import nl.sirrah.redditscreener.common.extensions.inflate
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
 import org.jetbrains.anko.find
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
-class OverviewFragment : Fragment() {
-    // TODO still necessary if we use Timber for logging?
-    val TAG = javaClass.simpleName
-
+class OverviewFragment : Fragment(), AnkoLogger {
     val redditService by lazy { RedditService.create() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
@@ -34,7 +32,7 @@ class OverviewFragment : Fragment() {
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe { listing ->
-                        Log.d(TAG, "Received: $listing")
+                        debug("Received: $listing")
                         linksList.adapter = LinkAdapter(listing.children);
                     }
         }
