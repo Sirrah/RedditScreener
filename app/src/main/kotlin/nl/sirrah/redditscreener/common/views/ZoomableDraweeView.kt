a618645000a6e3ea1ab7ad2473a0bee76b126889
Peter Oost <peteroost@sirrah.nl>
2016-06-23 10:52:51 +0200

    Add ZoomableDraweeView

---
+++ app/src/main/kotlin/nl/sirrah/redditscreener/common/views/ZoomableDraweeView.kt
@@ -0,0 +1,167 @@
+package nl.sirrah.redditscreener.common.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Matrix
+import android.util.AttributeSet
+import android.util.DisplayMetrics
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.view.WindowManager
+import com.facebook.drawee.view.SimpleDraweeView
+
+/**
+ * A SimpleDraweeView that supports Pinch to zoom.
+
+ * This class is converted to Kotlin from https://gist.github.com/nbarraille/eb5d0da20bc813969b08
+ */
+class ZoomableDraweeView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : SimpleDraweeView(context, attrs, defStyle) {
+    private val scaleDetector: ScaleGestureDetector
+    private val scaleListener: ScaleGestureDetector.OnScaleGestureListener
+    private val gestureDetector: GestureDetector
+
+    private val currentMatrix = Matrix()
+
+    private var currentScale = 0f
+    private var midX = 0f
+    private var midY = 0f
+
+    /**
+     * Setting the actionBarHeight is used when the actionbar is used in the application it will be
+     * used in calculating the y-transaction on panning, to be able to calculate the bottom of the
+     * screen correctly
+     */
+    var actionBarHeight: Int = 0
+
+    var onZoomChangeListener: OnZoomChangeListener? = null
+
+    init {
+        scaleListener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+            override fun onScale(detector: ScaleGestureDetector): Boolean {
+                var scaleFactor = detector.scaleFactor
+                val newScale = currentScale * scaleFactor
+
+                // Prevent from zooming out more than original
+                if (newScale > 1.0f && newScale < 4.0f) {
+                    // We initialize this lazily so that we don't have to register (and force the user
+                    // to unregister) a global layout listener on the view.
+                    if (midX == 0.0f) {
+                        midX = width / 2.0f
+                    }
+                    if (midY == 0.0f) {
+                        midY = height / 2.0f
+                    }
+                    currentScale = newScale
+                    currentMatrix.postScale(scaleFactor, scaleFactor, midX, midY)
+                    invalidate()
+                } else if (newScale < 1.0f) {
+                    scaleFactor = 1.0f / currentScale
+                    reset()
+                }
+
+                if (onZoomChangeListener != null && scaleFactor != 1.0f) {
+                    onZoomChangeListener!!.onZoomChange(currentScale)
+                }
+
+                return true
+            }
+        }
+        scaleDetector = ScaleGestureDetector(context, scaleListener)
+
+        gestureDetector = GestureDetector(context, GestureListener())
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        onZoomChangeListener = null
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        val saveCount = canvas.save()
+        canvas.concat(currentMatrix)
+        super.onDraw(canvas)
+        canvas.restoreToCount(saveCount)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        scaleDetector.onTouchEvent(event)
+        if (!scaleDetector.isInProgress) {
+            gestureDetector.onTouchEvent(event)
+        }
+        super.onTouchEvent(event)
+        return true
+    }
+
+    /**
+     * Resets the zoom of the attached image. This has no effect if the image has been destroyed
+     */
+    fun reset() {
+        currentMatrix.reset()
+        currentScale = 1.0f
+        invalidate()
+    }
+
+    internal inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+        override fun onScroll(e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+            var distanceX = distanceX
+            var distanceY = distanceY
+            //Only scroll when we are zoomed in
+            if (currentScale > 1) {
+                val values = FloatArray(9)
+                currentMatrix.getValues(values)
+
+                //Get the metrics for the display, used to detect edges for scrolling
+                val displayMetrics = DisplayMetrics()
+                val manager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+                manager.defaultDisplay.getMetrics(displayMetrics)
+
+                //Find the left and right limit for scrolling
+                if (distanceX < 0 && values[Matrix.MTRANS_X] > 0 || distanceX > 0 && values[Matrix.MTRANS_X] < -1 * (width * values[Matrix.MSCALE_X]) + displayMetrics.widthPixels) {
+                    distanceX = 0f
+                }
+
+                //Find the top and bottom limit for scrolling
+                if (distanceY < 0 && values[Matrix.MTRANS_Y] > 0 || distanceY > 0 && values[Matrix.MTRANS_Y] < -1 * (height * values[Matrix.MSCALE_Y]) + (displayMetrics.heightPixels - actionBarHeight)) {
+                    distanceY = 0f
+                }
+
+                currentMatrix.postTranslate(-distanceX, -distanceY)
+                invalidate()
+
+                return true
+            }
+
+            return false
+        }
+
+        override fun onDoubleTap(e: MotionEvent): Boolean {
+            val scaleFactor = 2.0f
+            if (currentScale == 0f) {
+                currentScale = 2.0f
+                currentMatrix.postScale(scaleFactor, scaleFactor, midX, midY)
+            } else if (currentScale < 4.0f) {
+                currentScale = 4.0f
+                currentMatrix.postScale(scaleFactor, scaleFactor, midX, midY)
+            } else {
+                currentScale = 0f
+                currentMatrix.reset()
+            }
+
+            invalidate()
+            return true
+        }
+    }
+
+    /**
+     * A listener interface for when the zoom scale changes
+     */
+    interface OnZoomChangeListener {
+        /**
+         * Callback method getting triggered when the zoom scale changes. This is not called when
+         * the zoom is programmatically reset
+         */
+        fun onZoomChange(scale: Float)
+    }
+}
--- app/src/main/kotlin/nl/sirrah/redditscreener/fragments/DetailFragment.kt
+++ app/src/main/kotlin/nl/sirrah/redditscreener/fragments/DetailFragment.kt
@@ -1,15 +1,20 @@
 package nl.sirrah.redditscreener.fragments
 
+import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.facebook.drawee.view.SimpleDraweeView
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.drawee.controller.BaseControllerListener
+import com.facebook.imagepipeline.image.ImageInfo
 import com.trello.rxlifecycle.components.support.RxFragment
 import nl.sirrah.redditscreener.R
 import nl.sirrah.redditscreener.common.extensions.inflate
-import nl.sirrah.redditscreener.common.extensions.setImageUri
+import nl.sirrah.redditscreener.common.extensions.snackbar
+import nl.sirrah.redditscreener.common.views.ZoomableDraweeView
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.find
 
@@ -27,7 +32,31 @@ class DetailFragment : RxFragment(), AnkoLogger {
         description = arguments?.get("description") as? String;
 
         // alternatively use the image property after the view has been fully loaded, i.e. in #onActivityCreated
-        (view.findViewById(R.id.image) as SimpleDraweeView).setImageUri(url)
+        val image: ZoomableDraweeView = view.find(R.id.image)
+
+        //Listener for the fresco image loader to react on success/failure
+        val controllerListener = object : BaseControllerListener<ImageInfo>() {
+            override fun onFinalImageSet(id: String?, imageInfo: ImageInfo?, animatable: Animatable?) {
+                //Get the height of the actionBar for the current theme. This height is used to
+                //determine the panning-height for the imageView.
+                val tv = TypedValue()
+
+                context.theme.resolveAttribute(R.attr.actionBarSize, tv, true)
+                val actionBarHeight = resources.getDimensionPixelSize(tv.resourceId)
+                image.actionBarHeight = actionBarHeight
+            }
+
+            override fun onFailure(id: String?, e: Throwable?) {
+                snackbar("Error while loading image: ${e?.message}")
+            }
+        }
+
+        val controller = Fresco.newDraweeControllerBuilder()
+                .setControllerListener(controllerListener)
+                .setUri(url)
+                .build()
+
+        image.setController(controller)
         return view
     }
 
