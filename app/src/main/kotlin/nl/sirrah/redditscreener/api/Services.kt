cb2e14696d39c2989b53f2d34646e25179b23706
Peter Oost <peteroost@sirrah.nl>
2017-05-31 10:35:47 +0200

    Some static imports and enable basic logging for normal builds

--- app/src/main/kotlin/nl/sirrah/redditscreener/api/Services.kt
+++ app/src/main/kotlin/nl/sirrah/redditscreener/api/Services.kt
@@ -1,10 +1,12 @@
 package nl.sirrah.redditscreener.api
 
-import com.google.gson.FieldNamingPolicy
+import com.google.gson.FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES
 import com.google.gson.GsonBuilder
 import nl.sirrah.redditscreener.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level.BASIC
+import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,19 +17,17 @@ object Services {
     val okHttpClient: OkHttpClient by lazy {
         val okHttpClientBuilder = OkHttpClient.Builder()
 
-        // Add logging when in debug mode
-        if (BuildConfig.DEBUG) {
-            val logger = HttpLoggingInterceptor()
-            logger.level = HttpLoggingInterceptor.Level.BODY
-            okHttpClientBuilder.addInterceptor(logger)
-        }
+        okHttpClientBuilder.addInterceptor(HttpLoggingInterceptor()
+                .apply {
+                    level = if (BuildConfig.DEBUG) BODY else BASIC
+                })
 
         okHttpClientBuilder.build()
     }
 
     val reddit: RedditApi by lazy {
         val gson = GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .setFieldNamingPolicy(LOWER_CASE_WITH_UNDERSCORES)
                 .registerTypeAdapterFactory(ItemTypeAdapterFactory())
                 .create()
 
