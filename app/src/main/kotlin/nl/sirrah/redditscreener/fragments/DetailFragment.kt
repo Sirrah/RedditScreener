6f2418780299a3bd4f6c6f3abefadfc83012b9ce
Peter Oost <peteroost@sirrah.nl>
2017-12-31 09:59:00 +0100

    Update dependencies, resolve resulting compile errors, update gitignore

--- app/src/main/kotlin/nl/sirrah/redditscreener/fragments/DetailFragment.kt
+++ app/src/main/kotlin/nl/sirrah/redditscreener/fragments/DetailFragment.kt
@@ -24,13 +24,15 @@ class DetailFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = container!!.inflate(R.layout.fragment_detail)
+        if (container == null) return null
+
+        val root = container.inflate(R.layout.fragment_detail)
 
         url = arguments?.get("url") as? String
         description = arguments?.get("description") as? String
 
         // alternatively use the image property after the view has been fully loaded, i.e. in #onActivityCreated
-        val image: ZoomableDraweeView = view.find(R.id.image)
+        val image: ZoomableDraweeView = root.find(R.id.image)
 
         //Listener for the fresco image loader to react on success/failure
         val controllerListener = object : BaseControllerListener<ImageInfo>() {
@@ -39,7 +41,7 @@ class DetailFragment : BaseFragment() {
                 //determine the panning-height for the imageView.
                 val tv = TypedValue()
 
-                context.theme.resolveAttribute(R.attr.actionBarSize, tv, true)
+                context?.theme?.resolveAttribute(R.attr.actionBarSize, tv, true)
                 val actionBarHeight = resources.getDimensionPixelSize(tv.resourceId)
                 image.actionBarHeight = actionBarHeight
             }
@@ -55,7 +57,7 @@ class DetailFragment : BaseFragment() {
                 .build()
 
         image.controller = controller
-        return view
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
