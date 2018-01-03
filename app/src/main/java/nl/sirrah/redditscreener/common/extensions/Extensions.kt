e97306a1b03e5acf3a13cc933b3ee8310095f13c
Peter Oost <peteroost@sirrah.nl>
2018-01-03 19:32:07 +0100

    Code cleanup

--- app/src/main/java/nl/sirrah/redditscreener/common/extensions/Extensions.kt
+++ app/src/main/java/nl/sirrah/redditscreener/common/extensions/Extensions.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.net.Uri
 import android.os.Build
 import android.support.design.widget.Snackbar
+import android.support.design.widget.Snackbar.LENGTH_SHORT
 import android.support.v4.app.Fragment
 import android.text.TextUtils
 import android.view.LayoutInflater
@@ -13,9 +14,8 @@ import com.facebook.drawee.view.SimpleDraweeView
 import kotlinx.android.synthetic.main.link_item.view.*
 import org.jetbrains.anko.find
 
-fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
-    return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
-}
+fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false)
+        = LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
 
 fun SimpleDraweeView.setImageUri(uri: String?) {
     if (!TextUtils.isEmpty(uri)) {
@@ -27,34 +27,34 @@ fun SimpleDraweeView.setImageUri(uri: String?) {
     }
 }
 
-fun View.snackbar(text: CharSequence, duration: Int = Snackbar.LENGTH_SHORT, init: Snackbar.() -> Unit = {}): Snackbar {
-    return Snackbar.make(this, text, duration).apply {
-        init()
-        show()
-    }
-}
+fun View.snackbar(text: CharSequence, duration: Int = LENGTH_SHORT, init: Snackbar.() -> Unit = {})
+        = Snackbar.make(this, text, duration)
+        .apply {
+            init()
+            show()
+        }
 
-fun View.snackbar(text: Int, duration: Int = Snackbar.LENGTH_SHORT, init: Snackbar.() -> Unit = {}): Snackbar {
-    return Snackbar.make(this, text, duration).apply {
-        init()
-        show()
-    }
-}
+fun View.snackbar(text: Int, duration: Int = LENGTH_SHORT, init: Snackbar.() -> Unit = {})
+        = Snackbar.make(this, text, duration)
+        .apply {
+            init()
+            show()
+        }
 
-fun Fragment.snackbar(text: CharSequence, duration: Int = Snackbar.LENGTH_SHORT, init: Snackbar.() -> Unit = {}): Snackbar =
-        view!!.snackbar(text, duration, init)
+fun Fragment.snackbar(text: CharSequence, duration: Int = LENGTH_SHORT, init: Snackbar.() -> Unit = {})
+        = view!!.snackbar(text, duration, init)
 
-fun Fragment.snackbar(text: Int, duration: Int = Snackbar.LENGTH_SHORT, init: Snackbar.() -> Unit = {}): Snackbar =
-        view!!.snackbar(text, duration, init)
+fun Fragment.snackbar(text: Int, duration: Int = LENGTH_SHORT, init: Snackbar.() -> Unit = {})
+        = view!!.snackbar(text, duration, init)
 
-fun Activity.snackbar(text: CharSequence, duration: Int = Snackbar.LENGTH_SHORT, init: Snackbar.() -> Unit = {}): Snackbar =
-        find<View>(android.R.id.content).snackbar(text, duration, init)
+fun Activity.snackbar(text: CharSequence, duration: Int = LENGTH_SHORT, init: Snackbar.() -> Unit = {})
+        = find<View>(android.R.id.content).snackbar(text, duration, init)
 
-fun Activity.snackbar(text: Int, duration: Int = Snackbar.LENGTH_SHORT, init: Snackbar.() -> Unit = {}): Snackbar =
-        find<View>(android.R.id.content).snackbar(text, duration, init)
+fun Activity.snackbar(text: Int, duration: Int = LENGTH_SHORT, init: Snackbar.() -> Unit = {})
+        = find<View>(android.R.id.content).snackbar(text, duration, init)
 
-fun isAPIVersionOrAbove(apiVersion: Int, func: () -> Unit ) {
-    if (Build.VERSION.SDK_INT >- apiVersion) {
+fun isAPIVersionOrAbove(apiVersion: Int, func: () -> Unit) {
+    if (Build.VERSION.SDK_INT > -apiVersion) {
         func()
     }
 }
